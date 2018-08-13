5d5de3b237feba0990b8a7f97f8480dd538e5ccf
Jasper Renow-Clarke <jasper@mrgsystems.co.uk>
2018-08-13 23:32:25 +0100

    Rearranged a bit

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-// Stop things from being dragged around
-window.ondragstart=function(event) { event.preventDefault(); };
-
-// Init
+// Initial entry point
 function init()
 {
+  /////////////////////////////////////////////////////
+  // Initialise stuff
+  // Stop things from being dragged around
+  window.ondragstart=function(event) { event.preventDefault(); };
+
+  /////////////////////////////////////////////////////
+  // Intro
   write("Connecting...");
   var dialler=new dtmf_dial;
   dialler.randomdial(10);
+
+  /////////////////////////////////////////////////////
+  // Main menu
+
+  /////////////////////////////////////////////////////
+  // Start game
 }
 
 // Run the init() once page has loaded
