c7a70de2bde6e40f3859012aac39f985e179ebf0
Jasper Renow-Clarke <jasper@mrgsystems.co.uk>
2018-08-13 17:14:27 +0100

    Stop things from being dragged around

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// Stop things from being dragged around
+window.ondragstart=function(event) { event.preventDefault(); };
+
+// Init
 window.onload=function() {
 var dialler=new dtmf_dial;
 dialler.randomdial(10);
