ef3e053e0d8a22f18bfa6dd88a6947d62234d8d7
Jasper Renow-Clarke <jasper@mrgsystems.co.uk>
2018-08-13 17:06:34 +0100

    Added CSS

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 zipfile="js13k.zip"
 buildpath="build"
 jscat="${buildpath}/min.js"
+csscat="${buildpath}/min.css"
 
 # Create clean build folder
 rm -Rf "${buildpath}" >/dev/null 2>&1
@@ -15,6 +16,9 @@ for file in random.js dtmf.js font.js main.js
 do
   yui-compressor "${file}" >> "${jscat}"
 done
+
+# Add CSS file
+yui-compressor main.css > "${csscat}"
 
 # Copy in the index file
 cp index.html "${buildpath}/"
