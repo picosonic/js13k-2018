96e9aea20afee7fd41d34c375f0fb25db87fd74f
Jasper Renow-Clarke <jasper@mrgsystems.co.uk>
2018-09-03 09:22:31 +0100

    Added more comments

--- timeline.js
+++ timeline.js
@@ -16,18 +16,22 @@ function timelineobj()
     this.timeline.sort(function(a,b) {return ((b.start<a.start)?1:(b.start==a.start)?0:-1)});
   };
 
+  // Animation frame callback
   this.timelineraf=function(timestamp)
   {
     var remain=0;
 
+    // If this is the first call then just record the epoch
     if (this.timelinepos==0)
     {
       this.timelineepoch=timestamp;
     }
     else
     {
+      // Calculate delta time since timeline start
       var delta=timestamp-this.timelineepoch;
 
+      // Look through timeline array for jobs not run which should have
       for (var i=0; i<this.timeline.length; i++)
       {
         if ((!this.timeline[i].done) && (this.timeline[i].start<delta))
@@ -36,19 +40,24 @@ function timelineobj()
           this.timeline[i].item();
         }
 
+        // Keep a count of all remaining jobs
         if (!this.timeline[i].done)
           remain++;
       }
 
+      // TODO - This should be a callback
       gs.writer.typechar();
     }
 
+    // Record new timeline position
     this.timelinepos=timestamp;
 
+    // If there is more jobs then request another callback
     if ((this.timelinepos==this.timelineepoch) || (remain>0))
       window.requestAnimationFrame(this.timelineraf.bind(this));
   };
 
+  // Start the timeline running
   this.begin=function()
   {
     window.requestAnimationFrame(this.timelineraf.bind(this));
